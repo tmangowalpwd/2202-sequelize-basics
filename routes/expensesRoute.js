d5b76c240b660a38bf25e1906852c19220423852
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-09-26 13:06:28 +0700

    exercise

--- routes/expensesRoute.js
+++ routes/expensesRoute.js
@@ -3,8 +3,17 @@ const expensesController = require('../controllers/expensesController')
 
 const router = express.Router()
 
+// 1. Ketika create expense, tidak terima UserId di body
+//    otomatis dapetin ID user yang sedang login
+//    lalu masukin ke UserId di setiap expense yang dibuat
 router.post("/", expensesController.createExpense)
+
+
 router.get("/total", expensesController.getTotalExpenses)
 router.post("/login", expensesController.loginUser)
+
+// 2. Dapetin list expenses dari user yang sedang login
+//    Jangan pake body, query, dan route params
+router.get("/me")
 
 module.exports = router
