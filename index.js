fd9cf9152e00799e9c2493a3b51fa02f52dcbf29
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-10-05 12:02:17 +0700

    redis cache

--- index.js
+++ index.js
@@ -12,28 +12,6 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const emailer = require("./lib/emailer")
-const handlebars = require("handlebars")
-app.post("/email", async (req, res) => {
-  // Baca email mentah
-  const rawHTML = fs.readFileSync("templates/register_user.html", "utf-8")
-  // Compile supaya bisa dipake handlebars
-  const compiledHTML = handlebars.compile(rawHTML)
-  // Isi variable2 yang ada di HTML
-  const result = compiledHTML({
-    username: "seto",
-  })
-
-  await emailer({
-    to: "voidfnc9@gmail.com",
-    html: result,
-    subject: "Test Email",
-    text: "Halo dunia",
-  })
-
-  res.send("Email sent")
-})
-
 const expensesRoute = require("./routes/expensesRoute")
 const authRoute = require("./routes/authRoute")
 const postsRoute = require("./routes/postsRoute")
@@ -44,18 +22,52 @@ const { verifyToken } = require("./middlewares/authMiddleware")
 app.use("/expenses", verifyToken, expensesRoute)
 app.use("/auth", authRoute)
 app.use("/posts", postsRoute)
-
-// Buat route /transaction
 app.use("/transactions", verifyToken, transactionsRoute)
 
 app.use("/public", express.static("public"))
 
-app.listen(PORT, () => {
+const axios = require("axios")
+const redisClient = require("./lib/redis")
+
+app.get("/dogs/:breed", async (req, res) => {
+  try {
+    const { breed } = req.params
+
+    const cacheResult = await redisClient.get(breed)
+
+    if (cacheResult) {
+      return res.status(200).json({
+        message: "Fetch dogs API",
+        data: JSON.parse(cacheResult)
+      })
+    }
+
+    const response = await axios.get(
+      `https://dog.ceo/api/breed/${breed}/images`
+    )
+
+    await redisClient.setEx(breed, 600, JSON.stringify(response.data))
+
+    return res.status(200).json({
+      message: "Fetch dogs API",
+      data: response.data,
+    })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      message: "Server error",
+    })
+  }
+})
+
+app.listen(PORT, async () => {
   db.sequelize.sync({ alter: true })
 
   if (!fs.existsSync("public")) {
     fs.mkdirSync("public")
   }
+
+  await redisClient.connect()
 
   console.log("Listening in port", PORT)
 })
